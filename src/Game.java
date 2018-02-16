500683972f9b94b6da01d54b0e6b6cf77e4d92a7
Gavin Frazar <gavinfrazar@umail.ucsb.edu>
2018-02-15 19:06:45 -0800

    RD/GF microcommit

---
+++ src/Game.java
@@ -0,0 +1,12 @@
+public abstract class Game
+{
+    //fields
+    bool player_won_;
+    
+    
+    //methods
+    public Game(); //honestly might not need since java gives one
+    public Game startGame();
+    
+    
+}
